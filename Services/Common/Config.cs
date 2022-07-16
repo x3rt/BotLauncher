74fa64ec35c7de3aa8aab3c063dd35a8396a1698
x3rt <official56zcs@gmail.com>
2022-07-15 21:16:58 -0600

    Remove unused config value

--- Services/Common/Config.cs
+++ Services/Common/Config.cs
@@ -7,7 +7,6 @@ namespace Bot_Launcher.Services.Common
         [JsonProperty("path-to-executable")] public string PathToExecutable { get; set; } = @"D:\coding\Kurva\KurvaBot\KurvaBot\bin\Debug\net6.0";
         [JsonProperty("executable")] public string Executable { get; set; } = "Bot.exe";
         [JsonProperty("exit-codes")] public ExitCodesConfig ExitCodes { get; set; } = new ExitCodesConfig();
-        [JsonProperty("restart-delay")] public int RestartDelay { get; set; } = 1;
         [JsonProperty("archive-to-extract")] public string ArchiveToExtract { get; set; } = "Latest.zip";
         [JsonProperty("debug")] public bool Debug { get; set; } = false;
 
